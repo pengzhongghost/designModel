a2aa0741779f3e64a50677773afd665a996e9688
kaonijiwa <189968924@qq.com>
2022-01-08 19:58:10 +0800

    原型模式

--- src/main/java/原型模式/深克隆浅克隆/Test.java
+++ src/main/java/原型模式/深克隆浅克隆/Test.java
@@ -7,6 +7,7 @@ public class Test {
      * todo 浅克隆是实现Cloneable接口，实现clone接口
      *      是一个新对象，但是属性的引用也引用过来了
      * todo 深克隆对clone方法重写，对对象对属性也进行克隆
+     * TODO 原型模式就是在工厂模式对基础上不去new工厂，只需要new出一个原型，然后通过clone来创建工厂
      */
     public static void main(String[] args) throws CloneNotSupportedException {
         Video video = new Video();
